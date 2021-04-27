8679650024142e897e0b0484a1e7d5fab3373447
Dinar <hudazi0158@gmail.com>
2021-04-27 19:04:27 +0400

    .

--- server.js
+++ server.js
@@ -12,14 +12,17 @@ server.use('/api/link', require('./routes/link.routes'))
 server.use('/t', require('./routes/redirect.routes'))
 
 if(process.env.NODE_ENV === 'production'){
-    server.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    server.use('/', express.static(path.join(__dirname, "/client/build")))
 
     server.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
-}
+}else{
+    server.get('/', (req, res) => {
+        res.send("Api run")
+    })
 
-const PORT = process.env || 5000
+const PORT = process.env.PORT || 5000
 
 async function start() {
     try {
